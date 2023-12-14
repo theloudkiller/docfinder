a44865c32f29ddf9d26140997b18b29952f84e36
theloudkiller <42111767+theloudkiller@users.noreply.github.com>
2023-12-14 11:01:34 +0530

    7
    7

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const path = require("path");
 
+//dotenv conig
 dotenv.config();
 
+//mongodb connection
 connectDB();
 
+//rest obejct
 const app = express();
 
-
+//middlewares
 app.use(express.json());
 app.use(moragan("dev"));
 
-
+//routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
@@ -27,11 +30,12 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+//port
 const port = process.env.PORT || 8080;
-
+//listen port
 app.listen(port, () => {
-    console.log(
-      `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
-        .bgCyan.white
-    );
-  });
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
